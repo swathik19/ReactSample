ea6a97a08c2eb9bac939eb8764dbcbe8424cb264
raji17mca <raji17mca@gmail.com>
2021-03-01 18:01:15 +0000

    Added filter and search option

--- src/Components/ValueAddList/TeamwiseSummaryTable/ValueAddListTableComponent.tsx
+++ src/Components/ValueAddList/TeamwiseSummaryTable/ValueAddListTableComponent.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { Card, CardBody, CardImg, CardText, CardTitle } from 'reactstrap';
+import { DefaultColumnFilter, SelectColumnFilter } from './Filter';
+import TableContainer from './TableContainer';
 import ValueAddListTableRowComponent from './ValueAddListTableRowComponents';
 
 interface IValueAddListTableComponentProps {
@@ -8,12 +11,85 @@ interface IValueAddListTableComponentProps {
 export default class ValueAddListTableComponent extends React.Component<IValueAddListTableComponentProps> {
 
   public render() {
+    const renderRowSubComponent = (row: { original: { name: { first: any; last: any; }; location: { city: any; street: any; postcode: any; }; picture: any; cell: any; }; }) => {
+      const {
+        name: { first, last },
+        location: { city, street, postcode },
+        picture,
+        cell,
+      } = row.original;
+      return (
+        <Card style={{ width: '18rem', margin: '0 auto' }}>
+          <CardImg top src={picture.large} alt='Card image cap' />
+          <CardBody>
+            <CardTitle>
+              <strong>{`${first} ${last}`} </strong>
+            </CardTitle>
+            <CardText>
+              <strong>Phone</strong>: {cell} <br />
+              <strong>Address:</strong>{' '}
+              {`${street.name} ${street.number} - ${postcode} - ${city}`}
+            </CardText>
+          </CardBody>
+        </Card>
+      );
+    };
+    
     return (
       <div className="container">
         <h2>Value-Adds List</h2>
-        {this.getTable()}
+        {/* {this.getTable()} */}
+        <TableContainer columns={this.getColumn()} data={this.props.details} renderRowSubComponent= {renderRowSubComponent}/>
       </div>
     );
+  }
+
+  private getColumn() {
+    const columns =  [
+        {
+          Header: 'Team',
+          accessor: 'Team'
+        },
+        {
+          Header: 'Primary POC',
+          accessor: 'Primary_POC',
+        },
+        {
+          Header: 'Date',
+          accessor: 'ProposedDate',
+        },
+        {
+          Header: 'Opportunity Title',
+          accessor: 'OpportunityTitle',
+        },
+        {
+          Header: 'Idea Title',
+          accessor: 'IdeaTitle',
+        },
+        {
+          Header: 'Client Portfolio',
+          accessor: 'ClientPortfolio',
+          Filter: SelectColumnFilter,
+          filter: 'equals' 
+        },
+        {
+          Header: 'Status',
+          accessor: 'Status',
+          Filter: SelectColumnFilter,
+          filter: 'equals' 
+        },
+        {
+          Header: 'Actions',
+          accessor: 'Actions',
+          DefaultColumnFilter : false,
+          disableFilters: true,
+          disableSortBy: true,
+          Cell: () => (<div> <a href=''> Edit </a> <a href=''> Delete </a></div>)
+        }
+        
+      ];
+
+    return columns;
   }
 
   private getTable(): JSX.Element {
--- src/Components/ValueAddSummary/ValueAddSummaryPageComponent.tsx
+++ src/Components/ValueAddSummary/ValueAddSummaryPageComponent.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ValueAddSummaryInformationComponent from './ValueAddSummaryInformation/ValueAddSummaryInformationComponent';
 import TeamwiseSummaryTableComponent from './TeamwiseSummaryTable/TeamwiseSummaryTableComponent';
-import TeamWiseSummaryChartComponent from './TeamwiseSummaryChart/TeamwiseSummaryChartComponent';
+import TeamWiseSummaryChartComponent from './TeamWiseSummaryChart/TeamWiseSummaryChartComponent';
 
 interface IValueAddSummaryPageComponentProps {
   details: any;
